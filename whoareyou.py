9fefccd519c78521b1e94d0550fe449365dd7a83
Axfrome <73723852+Axfrome@users.noreply.github.com>
2024-02-13 22:30:29 +0100

    Update whoareyou.py

--- whoareyou.py
+++ whoareyou.py
@@ -1,18 +1,20 @@
 import streamlit as st
 from exif import Image
+from streamlit_folium import st_folium
+import folium
 
 
 st.title('whoareyou') #le titre de l'app
 
 
 
-with open ('.\Staline.jpg', 'rb') as imageisfile:
-    itismyimage = Image(imageisfile)
+with open ('.\Staline.jpg', 'rb') as imageisfile: #initialise l'image staline
+    itismyimage = Image(imageisfile) # sur itismyimage
 
 
-st.image('.\Staline.jpg', width = 200,  caption='staline et non stallone')
+st.image('.\Staline.jpg', width = 200,  caption='staline et non stallone') #ajoute l'image staline au site
 
-
+#les lignes suivantes permettent l'ajout du formulaire et l'enregistrement des donnees sur la variable
 auteur = st.text_input("Auteur de l'image", key="Auteur")
 make = st.text_input("Fabriquant de l'appareil photo", key="Make")
 model = st.text_input("Model de l'appareil photo", key="Model")
@@ -28,24 +30,19 @@ pixelxdimension = st.text_input("Dimension Pixel X", key="Pixxdim")
 pixelydimension = st.text_input("Dimension Pixel y", key="Pixydim")
 
 
-
-#print(dir(itismyimage))
-
-
-# You can access the value at any point with:
-#st.session_state.Orientation
-
-#itismyimage.set('artist', 'saloperie')
+# 1er boutton test, permet de connaitre la list des EXIF sur l'image de base
 if st.button('test'):
     print("test")
     print(itismyimage.list_all())#savoir la list
 
+#2eme boutton test, permet d'afficher les EXIF sur staline modifier
 if st.button('test2'):
     print("test")
     with open ('.\Staline_modified.jpg', 'rb') as imageisfile:
         Coucou = Image(imageisfile)
         print(Coucou.list_all())
         
+# dernier boutton test, permet d'afficher les attributs EXIF enregistré dans l'image
 if st.button('test3'):
     print("test3")
     with open ('.\Staline_modified.jpg', 'rb') as imageisfile:
@@ -64,6 +61,7 @@ if st.button('test3'):
         print(Coucou.pixel_x_dimension)#imprime l'attribut
         print(Coucou.pixel_y_dimension)#imprime l'attribut
 
+#Ce boutton permet d'enregistrer les donnee entrée dans staline_modified.jpg
 if st.button('enregistrer dans staline_modified.jpg'):
     itismyimage.Artist = str(auteur) #ok
     print(itismyimage.Artist)
@@ -79,20 +77,27 @@ if st.button('enregistrer dans staline_modified.jpg'):
     print(itismyimage.iso_speed)
     itismyimage.datetime = str(datetimeoriginalimage)#ok
     print(itismyimage.datetime)
-    itismyimage.gps_altitude = int(gpsaltitude)#ok
+    itismyimage.gps_altitude = float(gpsaltitude)#ok
     print(itismyimage.gps_altitude)
-    itismyimage.gps_latitude = int(gpslatitude)#ok
+    itismyimage.gps_latitude = float(gpslatitude)#ok
     print(itismyimage.gps_latitude)
-    itismyimage.gps_longitude = int(gpslongitude)#ok
-    print(itismyimage.gps_Longitude)
+    itismyimage.gps_longitude = float(gpslongitude)#ok
+    print(itismyimage.gps_longitude)
     itismyimage.orientation = int(orientation)#ok
     print(itismyimage.orientation)
     itismyimage.pixel_x_dimension = int(pixelxdimension)#ok
     print(itismyimage.pixel_x_dimension)
     itismyimage.pixel_y_dimension = int(pixelydimension)#ok
     print(itismyimage.pixel_y_dimension)
-
-
     print("enregistré")
     with open('.\staline_modified.jpg', 'wb') as new_image_file:
         new_image_file.write(itismyimage.get_file())
+
+#reste le mapping :
+#if st.button('Carte'):
+with open ('.\Staline_modified.jpg', 'rb') as imageisfile:
+    mapimagegps = Image(imageisfile)
+    print(mapimagegps.gps_latitude) #test
+    print(mapimagegps.gps_longitude) #test
+m = folium.Map(location=[mapimagegps.gps_latitude, mapimagegps.gps_longitude], zoom_start=19) #test map
+st_data = st_folium(m, width=725) #ca a l'air de fonctionner
